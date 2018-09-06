ac0f6208e19bb65ac7b85be35a76f7f7812db036
Aaron Tapia <aarontr2409@gmail.com>
2018-09-05 23:31:10 -0500

    Lab1
    1. Programa en openGL para crear una linea.
    2. Programa para dibujar un poligono, que recibe como entrada el radio y el numero de lados.

---
+++ linea/main.cpp
@@ -0,0 +1,54 @@
+//#include <windows.h>  // for MS Windows
+#include <GL/glut.h>  // GLUT, include glu.h and gl.h
+#include <iostream>
+
+using namespace std;
+
+void reshape(int width, int height){
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, width, height, 0, 0, 1);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void line(int x1, int y1, int x2, int y2){
+    int x, y;
+    float a;
+    //int valor;
+    a=static_cast<float>(y2-y1)/static_cast<float>((x2-x1));
+    for (x=x1;x<=x2;x++){
+        y = static_cast<int>(y1 + a*(x - x1));
+        glVertex2f(x, y); //write_pixel(x, y, color);
+    }
+}
+
+void display() {
+   glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer (background)
+   glColor3f(1.0f, 0.0f, 0.0f); // Red
+   glLoadIdentity();
+
+   glBegin(GL_POINTS);
+    line(100, 150, 300, 250);
+   glEnd();
+
+   glFlush();  // Render now
+}
+
+void Init(){
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
+}
+
+/* Main function: GLUT runs as a console application starting at main()  */
+int main(int argc, char** argv) {
+   glutInit(&argc, argv);                 // Initialize GLUT
+   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+   glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
+   glutInitWindowSize(400, 400);   // Set the window's initial width & height
+   glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
+   Init();
+   glutDisplayFunc(display); // Register display callback handler for window re-paint
+   glutReshapeFunc(reshape);
+   glutMainLoop();           // Enter the event-processing loop
+   return 0;
+}
